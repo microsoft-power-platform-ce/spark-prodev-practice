692314b05caefb13f9e687b115e9d3ac588cd1b9
Melody Universe <melody.universe@microsoft.com>
2022-02-12 12:24:46 -0800

    created pipeline to test pac help

--- pwsh/Install-Pac.ps1
+++ pwsh/Install-Pac.ps1
@@ -1,20 +1,29 @@
-param (
-  [Parameter(Mandatory=$true)]
-  [string]$OperatingSystem,
-  [Parameter(Mandatory=$true)]
-  [string]$DestinationFolder,
-  [Parameter(Mandatory=$true)]
-  [string]$Platform
-)
-
 if (Get-Command pac -ErrorAction Ignore)
 {
   exit
 }
 
+if ([string]::IsNullOrEmpty($GITHUB_PATH))
+{
+  $platform = "azdops"
+} else {
+  $platform = "github"
+}
+
+switch($platform) {
+  "azdops" {
+    $destinationFolder = $env:AGENT_TEMPDIRECTORY
+    $operatingSystem = $env:AGENT_OS
+  }
+  "github" {
+    $destinationFolder = $env:RUNNER_TEMP
+    $operatingSystem = $env:RUNNER_OS
+  }
+}
+
 Write-Host "downloading pac..."
 
-$packageId = switch ($OperatingSystem) {
+$packageId = switch ($operatingSystem) {
   "Linux" { "Microsoft.PowerApps.CLI.Core.linux-x64" }
   { @("Darwin", "macOS") -contains $_ } {
     "Microsoft.PowerApps.CLI.Core.osx-x64"
@@ -29,45 +38,49 @@ $packageInfo = Invoke-RestMethod `
 $version = $packageInfo.items[0].upper
 Write-Host "latest version: $version"
 
-if(!(Test-Path $DestinationFolder)) {
-  New-Item $DestinationFolder `
+if(!(Test-Path $destinationFolder)) {
+  New-Item $destinationFolder `
     -ItemType Directory
 }
 
 Invoke-WebRequest `
   -Uri "https://api.nuget.org/v3-flatcontainer/$id/$version/$id.$version.nupkg" `
-  -OutFile "$DestinationFolder/$packageId.zip"
+  -OutFile "$destinationFolder/$packageId.zip"
 Write-Host "downloaded $packageId.zip"
 
 Expand-Archive `
-  "$DestinationFolder/$packageId.zip" `
-  "$DestinationFolder/$packageId"
+  "$destinationFolder/$packageId.zip" `
+  "$destinationFolder/$packageId"
 Write-Host "extracted to $packageId"
 
 Copy-Item `
-  "$DestinationFolder/$packageId/tools" `
-  "$DestinationFolder/pac" `
+  "$destinationFolder/$packageId/tools" `
+  "$destinationFolder/pac" `
   -Recurse
-Write-Host "copied tools subfolder to $DestinationFolder/pac"
+Write-Host "copied tools subfolder to $destinationFolder/pac"
 
 if (Get-Command chmod -ErrorAction Ignore) {
   Write-Host "making pac executable"
-  chmod 777 "$DestinationFolder/pac/pac"
+  chmod 777 "$destinationFolder/pac/pac"
 }
 
 Write-Host "disabling telemetry"
-Invoke-Expression "$DestinationFolder/pac/pac telemetry disable"
+Invoke-Expression "$destinationFolder/pac/pac telemetry disable"
 
 Write-Host "Prepending pac folder to system path"
-switch ($Platform) {
+switch ($platform) {
   "azdops" {
-    Write-Host "##vso[task.prependpath]$DestinationFolder/pac"
+    Write-Host "##vso[task.prependpath]$destinationFolder/pac"
   }
   "github" {
-    Write-Output "$DestinationFolder/pac" |
+    Write-Output "$destinationFolder/pac" |
       Out-File `
         -FilePath $env:GITHUB_PATH `
         -Encoding utf8 `
         -Append
   }
 }
+
+Set-Alias `
+  -Name pac `
+  -Value "$destinationFolder/pac/pac"
