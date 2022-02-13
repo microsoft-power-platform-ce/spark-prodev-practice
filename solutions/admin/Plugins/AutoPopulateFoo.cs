0083cebd3b7bdf68ba13112c45f9d5d244297e38
melody-universe <melody.universe@microsoft.com>
2022-02-12 16:52:58 -0800

    created plugins project

---
+++ solutions/admin/Plugins/AutoPopulateFoo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Cr90c73.Admin.Plugins
+{
+    public class AutoPopulateFoo : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(
+                typeof(IPluginExecutionContext)
+            );
+            var target = (Entity)context.InputParameters["Target"];
+            var random = new Random();
+            target["crc8e_foo"] = random.Next().ToString();
+        }
+    }
+}
